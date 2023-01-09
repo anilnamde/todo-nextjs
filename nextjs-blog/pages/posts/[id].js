9c0552d411aa38c482b0b3e2bd49353c516e3695
Anil Namde <anilnamde@gmail.com>
2023-01-10 00:35:18 +0400

    add html read from markdown

--- nextjs-blog/pages/posts/[id].js
+++ nextjs-blog/pages/posts/[id].js
@@ -8,7 +8,7 @@ export default function Post({ postData }) {
             <br />
             {postData.id}
             <br />
-            {postData.date}
+            <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
     )
 }
@@ -22,7 +22,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-    const postData = getPostData(params.id)
+    const postData = await getPostData(params.id)
     return {
         props: {
             postData
